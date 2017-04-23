fe761bc41c1616c45503fdb9072206a1c63f742c
jaus <sekiyahikarisi@yahoo.co.jp>
2017-04-23 21:46:54 +0900

    index

--- index.js
+++ index.js
@@ -99,35 +99,33 @@ app.get('/', function(request, response) {
                         toots_public.push(toot);
                     }
                     response.locals.toots_public = toots_public;
-                }
-            })
-
-            M.get('timelines/home', function(err, data, res) {
-                if(!err){
-                    for (key in data) {
-                        var toot = {
-                            id : data[key].account.username,
-                            profile_img : data[key].account.avatar,
-                            content : data[key].content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '')
-                        };
-                        toots_home.push(toot);
-                    }
-                    response.locals.toots_home = toots_home;
-                }
-            })
-
-            M.get('timelines/public?local=on', function(err, data, res) {
-                if(!err){
-                    for (key in data) {
-                        var toot = {
-                            id : data[key].account.username,
-                            profile_img : data[key].account.avatar,
-                            content : data[key].content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '')
-                        };
-                        toots_local.push(toot);
-                    }
-                    response.locals.toots_local = toots_local;
-                    response.render('pages/index');
+                    M.get('timelines/home', function(err, data, res) {
+                        if(!err){
+                            for (key in data) {
+                                var toot = {
+                                    id : data[key].account.username,
+                                    profile_img : data[key].account.avatar,
+                                    content : data[key].content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '')
+                                };
+                                toots_home.push(toot);
+                            }
+                            response.locals.toots_home = toots_home;
+                            M.get('timelines/public?local=on', function(err, data, res) {
+                                if(!err){
+                                    for (key in data) {
+                                        var toot = {
+                                            id : data[key].account.username,
+                                            profile_img : data[key].account.avatar,
+                                            content : data[key].content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '')
+                                        };
+                                        toots_local.push(toot);
+                                    }
+                                    response.locals.toots_local = toots_local;
+                                    response.render('pages/index');
+                                }
+                            })
+                        }
+                    })
                 }
             })
         }
